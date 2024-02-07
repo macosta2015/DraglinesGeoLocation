5cddecaada1e96ed2a65014e0ff1623b7226c6cc
Mario Acosta <macosta2015@my.fit.edu>
2024-02-07 18:19:23 -0500

    Code is running with US cooridnates

--- draglineslocation/src/components/GeoLocation.js
+++ draglineslocation/src/components/GeoLocation.js
@@ -1,37 +1,14 @@
 import React, { useState, useEffect } from 'react';
+import proj4 from 'proj4';
 
-// Function to convert latitude and longitude to UTM X and Y coordinates
-function latLonToUTM(latitude, longitude) {
-  // Constants for UTM conversion
-  const k0 = 0.9996; // Scale factor
-  const a = 6378137.0; // Semi-major axis of the ellipsoid (WGS84)
-  const eSquared = 0.00669438; // Square of the eccentricity of the ellipsoid
-  const lonOrigin = -183.0; // Central meridian of the zone (Zone 1)
+// Define the projection for WGS84 (latitude and longitude) and the Florida East zone
+proj4.defs("EPSG:4326", "+proj=longlat +datum=WGS84 +no_defs");
+proj4.defs("EPSG:2236", "+proj=tmerc +lat_0=24.33333333333333 +lon_0=-81 +k=0.999941177 +x_0=200000.0001016 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=us-ft +no_defs");
 
-  // Convert latitude and longitude to radians
-  const latRad = latitude * Math.PI / 180;
-  const lonRad = longitude * Math.PI / 180;
-
-  // Calculate UTM constants
-  const aSquared = Math.pow(a, 2);
-  const ePrimeSquared = eSquared / (1 - eSquared);
-  const nu = a / Math.sqrt(1 - eSquared * Math.sin(latRad) * Math.sin(latRad));
-  const T = Math.tan(latRad) * Math.tan(latRad);
-  const C = ePrimeSquared * Math.cos(latRad) * Math.cos(latRad);
-  const A = Math.cos(latRad) * (lonRad - (lonOrigin * Math.PI / 180));
-
-  // Calculate UTM coordinates
-  const M = a * ((1 - eSquared / 4 - 3 * eSquared * eSquared / 64 - 5 * eSquared * eSquared * eSquared / 256) * latRad
-              - (3 * eSquared / 8 + 3 * eSquared * eSquared / 32 + 45 * eSquared * eSquared * eSquared / 1024) * Math.sin(2 * latRad)
-              + (15 * eSquared * eSquared / 256 + 45 * eSquared * eSquared * eSquared / 1024) * Math.sin(4 * latRad)
-              - (35 * eSquared * eSquared * eSquared / 3072) * Math.sin(6 * latRad));
-  const easting = k0 * nu * (A + (1 - T + C) * Math.pow(A, 3) / 6
-                  + (5 - 18 * T + T * T + 72 * C - 58 * ePrimeSquared) * Math.pow(A, 5) / 120) + 500000.0;
-  const northing = k0 * (M + nu * Math.tan(latRad) * (Math.pow(A, 2) / 2
-                   + (5 - T + 9 * C + 4 * C * C) * Math.pow(A, 4) / 24
-                   + (61 - 58 * T + T * T + 600 * C - 330 * ePrimeSquared) * Math.pow(A, 6) / 720));
-
-  return { x: easting, y: northing };
+function latLonToStatePlane(latitude, longitude) {
+  // Convert from WGS84 (latitude and longitude) to State Plane Coordinate System (Florida East zone)
+  const coordinates = proj4("EPSG:4326", "EPSG:2236", [longitude, latitude]);
+  return { x: coordinates[0], y: coordinates[1] };
 }
 
 function GeoLocation({ fetching }) {
@@ -48,7 +25,7 @@ function GeoLocation({ fetching }) {
           position => {
             setLatitude(position.coords.latitude);
             setLongitude(position.coords.longitude);
-            const { x, y } = latLonToUTM(position.coords.latitude, position.coords.longitude);
+            const { x, y } = latLonToStatePlane(position.coords.latitude, position.coords.longitude);
             setX(x);
             setY(y);
           },
@@ -74,7 +51,7 @@ function GeoLocation({ fetching }) {
           {latitude && longitude && (
             <div>
               <p>Latitude: {latitude}, Longitude: {longitude}</p>
-              <p>X (UTM): {x.toFixed(2)}, Y (UTM): {y.toFixed(2)}</p>
+              <p>X (SPCS Florida East): {x.toFixed(2)}, Y (SPCS Florida East): {y.toFixed(2)}</p>
             </div>
           )}
         </div>
